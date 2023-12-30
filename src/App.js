ca159878d3a0bc09e806aeea120431d4b16f04f1
Avadh Kanaiya <avadhkanaiya@gmail.com>
2023-12-30 10:47:00 +0530

    My first basic react app named as TextUtils that is useful for utility to convert text in different form.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,114 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router, Route, Routes
+} from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState('light');
+  // new variable 
+  const [background, setBackground] = useState('light');
+  const [yellowMode, setYellowMode] = useState('light');
+  const [blueMode, setBlueMode] = useState('light');
+  const [redMode, setRedMode] = useState('light');
+  const [labelText, setLabelText] = useState('dark');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 3000)
+  }
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      setLabelText('light');
+      setBackground('dark');
+      setYellowMode('light');
+      setBlueMode('light');
+      setRedMode('light');
+      document.body.style.backgroundColor = '#2C3333';
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "TextUtils - Dark Mode";
+    } else {
+      setMode('light');
+      setLabelText('dark');
+      setBackground('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+      document.title = "TextUtils - Light Mode";
+    }
+  }
+
+  const toggleYellowMode = () => {
+    if (yellowMode === 'light') {
+      setYellowMode('warning');
+      setBackground('warning');
+      setBlueMode('light');
+      setMode('light');
+      setRedMode('light');
+      document.body.style.backgroundColor = '#F4CE14';
+    }
+    else {
+      setYellowMode('light');
+      setBackground('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
+  const togglePrimaryMode = () => {
+    if (blueMode === 'light') {
+      setBlueMode('primary');
+      setYellowMode('light');
+      setRedMode('light')
+      setMode('yellow');
+      setBackground('primary');
+      document.body.style.backgroundColor = '#2D4263';
+    }
+    else {
+      setBlueMode('light');
+      setBackground('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
+  const toggleRedMode = () => {
+    if (redMode === 'light') {
+      setRedMode('danger');
+      setBlueMode('light');
+      setYellowMode('light');
+      setMode('light');
+      setBackground('danger');
+      document.body.style.backgroundColor = '#FF6969';
+    }
+    else {
+      setRedMode('light');
+      setBackground('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar title={"TextUtils"} mode={mode} redMode={redMode} toggleRedMode={toggleRedMode} blueMode={blueMode} togglePrimaryMode={togglePrimaryMode} navbarBg={background} yellowMode={yellowMode} toggleYellowMode={toggleYellowMode} toggleMode={toggleMode} lableText={labelText}></Navbar >
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path='/' element={<TextForm showAlert={showAlert} heading="Enter the text below to analyze" mode={background}></TextForm>} />
+            <Route exact path='/about' element={<About />} />
+          </Routes>
+        </div >
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+    const handleUppercase = () => {
+        let upperText = text.toUpperCase();
+        setText(upperText);
+        props.showAlert('Your text has been converted into Uppercase', 'success');
+    }
+    const handleLowercase = () => {
+        let lowerText = text.toLowerCase();
+        setText(lowerText);
+        props.showAlert('Your text has been converted into Lowercase', 'success');
+    }
+    const handleClearText = () => {
+        let confirmClear = window.confirm('Are you sure you want to clear the text?')
+        if (confirmClear) {
+            setText('');
+            props.showAlert('Your text has been cleared', 'danger');
+        } else {
+            props.showAlert('Your text is as it is keep going', 'success');
+        }
+    }
+
+    const copyText = () => {
+        let textarea = document.getElementById('mybox');
+        textarea.select();
+        navigator.clipboard.writeText(textarea.value)
+        textarea.setSelectionRange(0, 0);
+        props.showAlert('Copied to clipboard', 'success');
+        // alert("Congrats your text has been copied");
+    }
+
+    const handleOnChange = (event) => {
+        setText(event.target.value)
+    }
+    const [text, setText] = useState('');
+    const [speaking, setSpeaking] = useState(false);
+
+    // speaking text
+    function speakText() {
+        if (text !== '') {
+            const synth = window.speechSynthesis;
+            let utterance = new SpeechSynthesisUtterance(text);
+            synth.speak(utterance);
+            setSpeaking(true);
+        }
+        else {
+            alert('Please enter a text to speak');
+        }
+    }
+
+    // stop speaking text
+    function stopSpeaking() {
+        const synth = window.speechSynthesis;
+        synth.cancel();
+        setSpeaking(false);
+    }
+    // speaking toggle button 
+    const handleToggle = () => {
+        if (!speaking) {
+            speakText();
+        } else {
+            stopSpeaking();
+        }
+    }
+    return (
+        <>
+            <div className={`container`} style={{ color: props.mode !== 'light' ? 'white' : 'black' }}>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className={`form-control bg-${props.mode}`} value={text} onChange={handleOnChange} style={{ color: props.mode !== 'light' ? 'white' : 'black' }} id="mybox" rows="8"></textarea>
+                </div>
+                <button className='btn btn-primary mx-2' onClick={handleUppercase}>Convert To Uppercase</button>
+                <button className='btn btn-primary mx-2' onClick={handleLowercase}>Convert To Lowerrcase</button>
+                <button className='btn btn-primary mx-2' onClick={copyText}>Copy Text</button>
+                <button className='btn btn-primary mx-2' onClick={handleToggle} id='speech'>{speaking ? 'Stop speech' : 'Hear Your Text'}</button>
+                <button className='btn btn-danger mx-2' onClick={handleClearText}>Clear Text</button>
+            </div >
+            <div className='container my-3' style={{ color: props.mode !== 'light' ? 'white' : 'black' }}>
+                <h2>Your text summary</h2>
+                <p>{text.split(" ").length} words, {text.length} characters, </p>
+                <p>{0.008 * text.split(" ").length}Minutes to read</p>
+                <h2>Preview</h2>
+                <p>{text.length > 0 ? text : "Enter the text into the above inputbox to preview it here"}</p>
+            </div>
+        </>
+    )
+}
